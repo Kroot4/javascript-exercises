d6ef2e00a8c56bacb753c30da5ace62ec6fe4c94
KRoot4 <KRoot4@protonmail.com>
2022-03-29 21:04:15 -0400

    update1

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -4,19 +4,19 @@ const sumAll = function(a, b) {
     if (a < 0 || b < 0 || typeof a != 'number' || typeof b != 'number') {
         return 'ERROR';
 
-    } else if (a < b) {
+    } else {
+        if (a > b) {
+            let dum = a;
+            a = b;
+            b = dum;
+        }
+
         for (let i = a;i <= b; i++) {
             sum += i;
         }
         return sum;
-
-    } else {
-        for (let i = b;i <= a; i++) {
-            sum += i;
-        }
-        return sum;
     }
-    
+               
 };
 
 // Do not edit below this line
